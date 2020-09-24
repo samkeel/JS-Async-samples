9eca30a3569096fb2e1c667e859b6091c32f2111
Sam <programming@vectors.net.au>
2020-09-24 22:01:36 +0800

    promises added

--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,23 @@
-const getTodos = (callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource, callback) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', () => {
-        // console.log(request, request.readyState);
-        if (request.readyState === 4 && request.status === 200) {
-            // console.log(request.responseText);
-            callback(undefined, request.responseText);
-        } else if (request.readyState === 4) {
-            // console.log('could not fetch data');
-            callback('could not fetch', undefined);
-        }
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('error getting resource');
+            }
+        });
+
+        request.open('GET', resource)
+        request.send();
     });
-
-    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.send();
 };
 
-console.log(1);
-console.log(2);
-
-getTodos((err, data) => {
-    // console.log(err, data);
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
-
-
-console.log(3);
-console.log(4);
-
-//  https://jsonplaceholder.typicode.com/
+getTodos('todos/raiden.json').then(data => {
+    console.log('promise resolved:', data);
+}).catch(err => {
+    console.log('promise rejected:', err)
+})
